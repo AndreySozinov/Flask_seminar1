e907e1180afaa0145a665fe67044598ae097cd9f
AndreySozinov <savrey@ya.ru>
2024-07-17 18:15:04 +0500

    newHomework

--- homework.py
+++ homework.py
@@ -10,17 +10,26 @@ def shop_page():
 
 @app.route('/clothes/')
 def page_clothes():
-    return render_template('clothes.html')
+    _clothes = [{'size':['small clothes', 'medium clothes', 'large clothes']}]
+    context = {'title': 'Одежда',
+               'clothes': _clothes}
+    return render_template('clothes.html', **context)
 
 
 @app.route('/shoes/')
 def page_shoes():
-    return render_template('shoes.html')
+    _shoes = [{'type': ['slippers', 'boots', 'monks']}]
+    context = {'title': 'Обувь',
+               'shoes': _shoes}
+    return render_template('shoes.html', **context)
 
 
 @app.route('/jacket/')
 def page_jacket():
-    return render_template('jacket.html')
+    _jacket = [{'color': ['red color', 'white color', 'black color']}]
+    context = {'title': 'Куртки',
+               'jacket': _jacket}
+    return render_template('jacket.html', **context)
 
 
 if __name__ == '__main__':
